0e37ec22154cfbdb14efa34a9c69151efed965b0
ShinhwanKang <ksh981214@korea.ac.kr>
2020-05-29 03:17:26 +0900

    First Trainig Code

--- kobert/train.py
+++ kobert/train.py
@@ -46,13 +46,13 @@ t_total = len(train_dataloader) * config.num_epochs
 warmup_step = int(t_total * config.warmup_ratio)
 
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
-#scheduler = WarmupLinearSchedule(optimizer, warmup_steps=warmup_step, t_total=t_total)
 
 print("Start Training")
 from datetime import datetime
 
 best_epoch=0
 best_accuracy=-1
+warmup_epoch = 5
 
 for e in range(config.num_epochs):
     start = datetime.now()
@@ -61,24 +61,24 @@ for e in range(config.num_epochs):
     train_acc = 0.0
     test_acc = 0.0
     model.train()
-    '''
-        for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(train_dataloader):
-            optimizer.zero_grad()
-            token_ids = token_ids.long().to(device)
-            segment_ids = segment_ids.long().to(device)
-            valid_length= valid_length
-            label = label.long().to(device)
-            out = model(token_ids, valid_length, segment_ids)
-            loss = loss_fn(out, label)
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), config.max_grad_norm)
-            optimizer.step()
-            scheduler.step()  # Update learning rate schedule
-            train_acc += calc_accuracy(out, label)
-            if batch_id % config.log_interval == 0:
-                print("epoch {} batch id {} loss {} train acc {}".format(e+1, batch_id+1, loss.data.cpu().numpy(), train_acc / (batch_id+1)))
-        print("epoch {} train acc {}".format(e+1, train_acc / (batch_id+1)))
-    '''
+
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(train_dataloader):
+        optimizer.zero_grad()
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length= valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        loss = loss_fn(out, label)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), config.max_grad_norm)
+        optimizer.step()
+        scheduler.step()  # Update learning rate schedule
+        train_acc += calc_accuracy(out, label)
+        if batch_id % config.log_interval == 0:
+            print("epoch {} batch id {} loss {} train acc {}".format(e+1, batch_id+1, loss.data.cpu().numpy(), train_acc / (batch_id+1)))
+    print("epoch {} train acc {}".format(e+1, train_acc / (batch_id+1)))
+
     model.eval()
     #for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm(test_dataloader)):
     for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
@@ -91,20 +91,11 @@ for e in range(config.num_epochs):
     test_acc = test_acc / (batch_id+1)
     print("epoch {} test acc {}".format(e+1, test_acc))
 
-    if best_accuracy < test_acc:
+    if best_accuracy < test_acc and e >= warmup_epoch:
         best_epoch = e
         best_accuracy = test_acc
         #Model save
         torch.save(model.state_dict(), "./model/model.epoch-" + str(e))
-
-        # #kaggle
-        # kaggle_id, kaggle_sen, kaggle_label=[],[],[]
-        # with open(config.kaggle_path,'r', encoding='euc-kr') as f:
-        #     rdr = csv.DictReader(f)
-        #     for i in rdr:
-        #         kaggle_id.append(i['Id'])
-        #         kaggle_sen.append(i['Sentence'])
-
         #make pred
         wf = open("./result/sample_epoch_"+str(e)+".csv",'w', encoding='euc-kr')
         wr=csv.writer(wf)
@@ -115,5 +106,5 @@ for e in range(config.num_epochs):
             valid_length= valid_length
             out = model(token_ids, valid_length, segment_ids)
             _, max_indices = torch.max(out,1)
-            wr.writerow([batch_id,max_indices])
+            wr.writerow([batch_id,max_indices.item()])
         wf.close()
